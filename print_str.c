13b52d723214d8f5f00a54a443efc1bf2ff3b804
nourchaira <nour.chaira@gmail.com>
2023-11-26 21:32:31 +0100

    print_str

--- print_str.c
+++ print_str.c
@@ -1,31 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen - finds the lenght of a string.
+ * _strlen - finds the length of a string.
  * @str: string pointer
- * Return: i.
+ * Return: length of the string.
  */
-
 int _strlen(char *str)
 {
-	int i;
+    int i;
 
-	for (i = 0; str[i] != 0; i++)
-		;
-	return (c);
-}
+    for (i = 0; str[i] != '\0'; i++)
+        ;
 
-/**
- * _strlenc - finds length of a string but for constant characters
- * @str: char pointer
- * Return: i
- */
-
-int _strlenc(const char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != 0; i++)
-		;
-	return (i);
+    return i;
 }
