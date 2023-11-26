79a81fc9da77030da14cb5b632d4bae1183bdcc5
nourchaira <nour.chaira@gmail.com>
2023-11-26 21:29:47 +0100

    main.h

--- main.h
+++ main.h
@@ -1,4 +1,3 @@
-// main.h
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -19,6 +18,6 @@ int _strlenc(const char *str);
 int print_37(void);
 int print_i(va_list args);
 int print_d(va_list args);
-int print_percent(va_list args); // Added function prototype
+int print_percent(va_list args);
 
 #endif
