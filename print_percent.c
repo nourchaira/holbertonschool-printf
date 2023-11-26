a9ce87a1a9f9a7cdbfe799c2b3d45f742a533732
nourchaira <nour.chaira@gmail.com>
2023-11-26 21:27:00 +0100

    print_percent

---
+++ print_percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_percent - prints the character '%'.
+ * @args: unused argument.
+ * Return: 1 (the number of characters printed).
+ */
+int print_percent(va_list args)
+{
+    (void)args; 
+    _putchar('%');
+    return 1;
+}
