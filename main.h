f07ccd422399e1774c18ce891f7bd8ecf8c49130
nourchaira <nour.chaira@gmail.com>
2023-11-27 16:33:47 +0100

    printf

--- main.h
+++ main.h
@@ -14,7 +14,7 @@ int printf_char(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _strlen(char *str);
-int printf_string(va_list val);
+int printf_string(va_list args);
 int _strlenc(const char *str);
 int print_37(void);
 int print_i(va_list args);
